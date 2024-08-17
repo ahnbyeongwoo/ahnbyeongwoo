7379cfb7e9950e0cded2d6e4a4675389c32ca6c6
ahnbyeongwoo <162805813+ahnbyeongwoo@users.noreply.github.com>
2024-08-18 00:16:11 +0900

    Update app.js

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defalutResult = 0;
 let currentResult = defalutResult;
+let logEntries=[];
 
 function getUserNumberInput() {
     return parseInt(userInput.value);
@@ -10,35 +11,54 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     outputResult(currentResult, calcDescritption);
 }
 
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,//함수에서 받는 매개 변수로 작업
+    newResult,){
+    const logEntry={
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult,
+      };
+      logEntries.push(logEntry);//새로운 배열에 요소 추가
+      console.log(logEntries);
+
+ }
+
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult=currentResult;
     currentResult = currentResult + enteredNumber;
     createAndWriteOutput('+',initialResult,enteredNumber); //두번째 매개변수는 실행하기 전의 결과
+    writeToLog('ADD',initialResult,enteredNumber,currentResult);
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult=currentResult;
-    currentResult = currentResult - enteredNumber;
-    createAndWriteOutput('-',initialResult,enteredNumber); //두번째 매개변수는 실행하기 전의 결과
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-',initialResult,enteredNumber); 
+    writeToLog('SUBTRACT',initialResult,enteredNumber,currentResult);
 }
-
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult=currentResult;
     currentResult = currentResult * enteredNumber;
-    createAndWriteOutput('*',initialResult,enteredNumber); //두번째 매개변수는 실행하기 전의 결과
+    createAndWriteOutput('*',initialResult,enteredNumber);
+    writeToLog('MULTIPLY',initialResult,enteredNumber,currentResult);
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult=currentResult;
     currentResult = currentResult / enteredNumber;
-    createAndWriteOutput('/',initialResult,enteredNumber); //두번째 매개변수는 실행하기 전의 결과
+    createAndWriteOutput('/',initialResult,enteredNumber); 
+    writeToLog('DIVIDE',initialResult,enteredNumber,currentResult);
 }
 
-addBtn.addEventListener('click', add);//스닛펫은 버튼을 클릭하면 실행해
+addBtn.addEventListener('click', add);//클릭 이벤트 발생
 subtractBtn.addEventListener('click',subtract);
 multiplyBtn.addEventListener('click',multiply);
 divideBtn.addEventListener('click',divide);
