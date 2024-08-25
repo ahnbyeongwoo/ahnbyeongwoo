ec7bbef7e2ac2cbdcb0b56ab7abaeef36be26224
ahnbyeongwoo <162805813+ahnbyeongwoo@users.noreply.github.com>
2024-08-25 18:34:52 +0900

    Add files via upload

---
+++ arrays-01-starting-code/app.js
@@ -0,0 +1,21 @@
+/*const numbers=[1,2,3];
+console.log(numbers);
+
+const moreNumbers=new Array(5);
+console.log(moreNumbers);
+
+const yetMoreNumbers=Array.of(1,2);
+console.log(yetMoreNumbers);
+
+const listItems=document.querySelectorAll('li');
+console.log(listItems);
+*/
+const hobbies=['Sports','Cooking'];
+hobbies.push('Reading');
+console.log(hobbies);
+hobbies.pop();
+console.log(hobbies);
+
+
+hobbies.splice(0,0,0);
+
